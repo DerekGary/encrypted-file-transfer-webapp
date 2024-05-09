c67950210ef2bb2839ea03107efcb156a4beba41
Derek Gary <jgary@uccs.edu>
2024-05-08 23:28:30 -0600

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,12 +1,12 @@
 # Authors: Derek Gary, Takaiya Jones
 
 # Purpose: Serves as the project level routing.
-# First point that is hit for requests.
+# First route that is hit for requests.
 # Routes data to our 'core' application for further
 # routing.
 
 from django.urls import path, include
-from views import index  # Adjusted import depending on your project structure
+from views import index
 
 urlpatterns = [
     path('api/', include('core.urls'), name='api'),
